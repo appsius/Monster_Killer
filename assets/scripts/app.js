4faa6f42779e98ac70413f5848c89842d98c32ae
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-05 05:21:08 +0300

    Controlling Bonus Life

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,12 +6,22 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
 function endRound() {
+	const initialPlayerHealth = currentPlayerHealth;
 	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
 	currentPlayerHealth -= playerDamage;
+
+	if (currentPlayerHealth <= 0 && hasBonusLife) {
+		hasBonusLife = false;
+		removeBonusLife();
+		currentPlayerHealth = initialPlayerHealth;
+		setPlayerHealth(initialPlayerHealth);
+		alert('You will dead if BonuLife does not exist');
+	}
 
 	if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
 		alert('YOU WON!');
